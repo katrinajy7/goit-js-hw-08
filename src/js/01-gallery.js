f32df70fcc1663f3f0b210d3f15740b23f5559a2
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-06-29 11:17:35 -0700

    UPDATED

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector("ul.gallery");
+
+const photosMarkup = createGalleryItem(galleryItems);
+
+function createGalleryItem(array) {
+   return array
+   .map(({ preview, original, description }) => {
+        return `
+        <a class='gallery_item' href="${original}">
+            <img class="gallery_image" src="${preview}" alt="${description}"/>
+        </a>
+        `;
+    })
+
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", photosMarkup);
+
+const galleryHandler = new simpleLightbox(".gallery a", { captionsData: "alt", captionsDelay: 250});
+console.log(galleryHandler);
